26bfb1f31908787f536d17c8b400273ac8d9ebee
Momo Ozawa & Yvonne Peng <student@epicodus.com>
2015-09-16 10:58:16 -0700

    Pass mocha test for wordFrequency

--- spec/specs.js
+++ spec/specs.js
@@ -1,3 +1,11 @@
+describe("wordFrequency", function() {
+  it("returns word frequncy in the order they appear", function() {
+    var text = "Java is fun. But I think JavaScript is also fun. Yay java and javascript!";
+    var hash = {java: 2, is: 2, fun: 2, but: 1, i: 1, think: 1, javascript: 2, also: 1, yay: 1, and: 1};
+    expect(wordFrequency(text)).to.eql(hash);
+  });
+});
+
 describe("convertTextToArray", function() {
   it("returns only words split into an array", function() {
     var text = "Stackoverflow's users... are, awesome!!!";
